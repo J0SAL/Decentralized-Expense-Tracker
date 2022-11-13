fada2d4f591af2a29373ad80ae0c3d78545f1d14
Josal <joyalmeida321@gmail.com>
2022-11-13 22:47:25 +0530

    added veriy contract script

--- backend/scripts/deploy.ts
+++ backend/scripts/deploy.ts
@@ -6,7 +6,30 @@ async function main() {
     const expenseStorage = await ExpenseTrackerFactory.deploy()
     await expenseStorage.deployed()
     console.log("Contract deployed to:", expenseStorage.address)
+
+
+    // verifying the contract on test net
+    if (network.config.chainId === 5 && process.env.ETHERSCAN_API_KEY) {
+        await expenseStorage.deployTransaction.wait(6)
+        await verify(expenseStorage.address, [])
+    }
 }
+
+const verify = async (contractAddress: string, args: any[]) => {
+    console.log("Verifying contract...")
+    try {
+      await run("verify:verify", {
+        address: contractAddress,
+        constructorArguments: args,
+      })
+    } catch (e: any) {
+      if (e.message.toLowerCase().includes("already verified")) {
+        console.log("Already Verified!")
+      } else {
+        console.log(e)
+      }
+    }
+  }
 
 main()
     .then(() => process.exit(0))
