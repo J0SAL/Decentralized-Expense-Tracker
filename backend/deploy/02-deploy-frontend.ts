eee852d028b32801712391d30afa5294e2c16787
Josal <joyalmeida321@gmail.com>
2022-11-29 20:36:49 +0530

    added script for abi and contract address

---
+++ backend/deploy/02-deploy-frontend.ts
@@ -0,0 +1,36 @@
+import { frontEndContractsFile, frontEndAbiFile } from "../helper-hardhat-config"
+import { network, ethers } from "hardhat"
+import { readFileSync, writeFileSync } from "fs"
+
+//this is const async arrow function
+const main = async () => {
+    if (process.env.UPDATE_FRONT_END) {
+        console.log("Writing to front end...")
+        await updateContractAddresses()
+        await updateAbi()
+        console.log("Front end written!")
+    }
+}
+
+// update api
+async function updateAbi() {
+    const expenseTracker: any = await ethers.getContract("ExpenseTracker")
+    writeFileSync(frontEndAbiFile, expenseTracker.interface.format(ethers.utils.FormatTypes.json))
+}
+
+async function updateContractAddresses() {
+    const expenseTracker = await ethers.getContract("ExpenseTracker")
+    const contractAddresses = JSON.parse(readFileSync(frontEndContractsFile, "utf8"))
+    if (network.config.chainId!.toString() in contractAddresses) {
+        if (
+            !contractAddresses[network.config.chainId!.toString()].includes(expenseTracker.address)
+        ) {
+            contractAddresses[network.config.chainId!.toString()].push(expenseTracker.address)
+        }
+    } else {
+        contractAddresses[network.config.chainId!.toString()] = [expenseTracker.address]
+    }
+    writeFileSync(frontEndContractsFile, JSON.stringify(contractAddresses))
+}
+
+export default main
--- backend/helper-hardhat-config.ts
+++ backend/helper-hardhat-config.ts
@@ -1,13 +1,11 @@
-
-  export interface networkConfigInfo {
+export interface networkConfigInfo {
     [key: string]: {
         name?: string
         blockConfirmations?: number
-      }
-  }
+    }
+}
 
-
-  export const networkConfig: networkConfigInfo = {
+export const networkConfig: networkConfigInfo = {
     default: {
         name: "hardhat",
     },
@@ -16,12 +14,15 @@
     },
     1: {
         name: "mainnet",
-        blockConfirmations: 1, 
+        blockConfirmations: 1,
     },
     5: {
         name: "goerli",
         blockConfirmations: 1,
     },
-  }
+}
 
-  export const developmentChains = ["hardhat", "localhost"]
+export const frontEndContractsFile = "../../constants/contractAddresses.json"
+export const frontEndAbiFile = "../../constants/abi.json"
+
+export const developmentChains = ["hardhat", "localhost"]
