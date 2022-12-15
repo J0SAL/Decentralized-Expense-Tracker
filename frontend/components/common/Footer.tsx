3f11a06a2feaafe36f87478fe6a6406ded39c137
Josal <joyalmeida321@gmail.com>
2022-12-16 02:35:26 +0530

    updated footer & navbar

--- frontend/components/common/Footer.tsx
+++ frontend/components/common/Footer.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div
+      className="py-3 d-flex justify-content-center"
+      style={{
+        backgroundColor: "rgba(0, 0, 0, 0.70)", //"rgb(232, 232, 232)"
+        color: "white",
+      }}
+    >
+      <h6>Made by Joy Almeida</h6>
+    </div>
+  );
 }
 
 export default Footer;
--- frontend/components/common/PageNavbar.tsx
+++ frontend/components/common/PageNavbar.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Container, Navbar } from "react-bootstrap";
 
 function PageNavbar() {
-  return <div>PageNavbar</div>;
+  return (
+    <>
+      <Navbar bg="light" variant="light" fixed="top">
+        <Container className="d-flex justify-content-center">
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="/logo.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{" "}
+            Expense Tracker
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default PageNavbar;
