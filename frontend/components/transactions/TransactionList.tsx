a8165945fa58398cbea68f721e4da671e3816452
Josal <joyalmeida321@gmail.com>
2022-12-16 03:24:27 +0530

    added transaction block

---
+++ frontend/components/transactions/TransactionList.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+var transactions = [
+  {
+    id: 1,
+    title: "Salary",
+    amount: 10000,
+    type: "income",
+    date: "2021-07-01T00:00:00.000Z",
+  },
+  {
+    id: 2,
+    title: "Salary",
+    amount: 10000,
+    type: "income",
+    date: "2021-07-01T00:00:00.000Z",
+  },
+  {
+    id: 3,
+
+    title: "Salary",
+    amount: 10000,
+    type: "income",
+    date: "2021-07-01T00:00:00.000Z",
+  },
+];
+function TransactionList() {
+  return <div>TransactionList</div>;
+}
+
+export default TransactionList;
--- frontend/components/transactions/Transactions.tsx
+++ frontend/components/transactions/Transactions.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import ExpenseChart from "./ExpenseChart";
+import IncomeChart from "./IncomeChart";
+import TransactionList from "./TransactionList";
 
 function Transactions() {
-  return <div>Transactions</div>;
+  return (
+    <Container style={{ minHeight: "100vh" }}>
+      <h3 id="transactions" className="d-flex justify-content-center my-3">
+        Transactions
+      </h3>
+      <Row gx={10}>
+        <Col md={4} xs={12}>
+          <div style={{ width: "90%" }}>
+            <IncomeChart />
+          </div>
+        </Col>
+        <Col md={4} xs={12}>
+          <div>
+            <TransactionList />
+          </div>
+        </Col>
+        <Col md={4} xs={12}>
+          <div style={{ width: "90%" }}>
+            <ExpenseChart />
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Transactions;
