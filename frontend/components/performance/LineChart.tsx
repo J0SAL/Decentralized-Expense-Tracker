d7a89361f1b7a3db7e8259fdab60e63b807c3761
Josal <joyalmeida321@gmail.com>
2022-12-21 01:48:42 +0530

    add performance graph with static data

---
+++ frontend/components/performance/LineChart.tsx
@@ -0,0 +1,69 @@
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+  Filler
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+    title: {
+      display: false,
+    },
+  },
+};
+
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Transactions",
+      data: labels.map(() => Math.random() * 100),
+      fill: true,
+      lineTension: 0.3,
+      backgroundColor: "#d6b0f5",
+      borderColor: "#9647d6",
+    },
+  ],
+};
+
+function LineChart() {
+  return <Line data={data} options={options} />;
+}
+export default LineChart;
--- frontend/components/performance/PerformanceGraph.tsx
+++ frontend/components/performance/PerformanceGraph.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import LineChart from "./LineChart";
 
 function PerformanceGraph() {
-  return <div>PerformanceGraph</div>;
+  return (
+    <Container>
+      <h1 className="text-center">Perfomance</h1>
+      <div className="mx-5 px-5">
+        <LineChart />
+      </div>
+    </Container>
+  );
 }
 
 export default PerformanceGraph;
