ed237984133b741b1026a254025e4d3897eb44e4
Ojas Aklecha <ojasaklechayt@gmail.com>
2023-05-24 23:35:35 +0530

    Improved the home page design for Large Screen users

--- frontend/components/common/Login.tsx
+++ frontend/components/common/Login.tsx
@@ -15,14 +15,19 @@ function Login() {
           alignItems: "center",
           gap: "20px",
         }}
-      >
-        <img
-          src="/images/welcome.svg"
-          alt="no data"
-          width={"100vh"}
-          height={"100vh"}
-        />
-        <ConnectButton moralisAuth={false} />
+      ><div className="inner-home"
+        style={{ display: "flex", justifyContent: "center", textAlign: "center", alignItems: 'center', flexDirection: "row", gap: "10vh" }}>
+          <h1 style={{ padding: "50px 5px" }}>Welcome to Expense Tracker</h1>
+          <img
+            src="/images/welcome.svg"
+            alt="no data"
+            width={"300vh"}
+            height={"300vh"}
+          />
+        </div>
+        <ConnectButton style={{
+          height: "7vh"
+        }} moralisAuth={false} />
       </div>
     </div>
   );
