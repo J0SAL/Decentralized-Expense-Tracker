630c4eaee277eaac4c7f61d79271db42088a6248
Abishek Upadhyay <98577878+Google-Ka-Dinosaur@users.noreply.github.com>
2023-05-30 22:47:58 +0530

    Update 404.tsx

--- frontend/pages/404.tsx
+++ frontend/pages/404.tsx
@@ -2,14 +2,15 @@ import React from "react";
 import Link from "next/link";
 import { Button } from "react-bootstrap";
 import classes from "../styles/error.module.css";
+
 const hello = () => {
   return (
     <>
       <div className={classes.imageBox}>
         <img
           className={classes.image}
-          src="https://i.ibb.co/G39013c/img-3.png"
-          alt="img-3"
+          src="/images/errorImg.png"
+          alt="timer"
         />
       </div>
       <div className={classes.button}>
