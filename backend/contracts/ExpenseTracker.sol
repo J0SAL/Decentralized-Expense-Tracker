13c80e4d4f2692461a03e51bccc49055073167ce
Mandal-Arpan2002 <100986138+Arpaninformationtech@users.noreply.github.com>
2023-05-31 12:51:46 +0530

    Optimize Smart Contract to reduce gas fees
    I have incorporated the changes as said by @J0SAL also incorporated the changes proposed by @shikhar229169

--- backend/contracts/ExpenseTracker.sol
+++ backend/contracts/ExpenseTracker.sol
@@ -6,8 +6,9 @@ contract ExpenseTracker {
         INCOME,
         EXPENSE
     }
+    
     struct Transaction {
-        string id;
+        uint256 id;
         TransactionType ttype;
         string category;
         string description;
@@ -17,24 +18,29 @@ contract ExpenseTracker {
     }
 
     struct Tracker {
-        uint total;
-        uint deleted;
-        string[] transactions;
+        uint256 total;
+        uint256 deleted;
+        uint256 counter;
+        mapping(uint256 => string) transactions;
     }
-    mapping(address => mapping(string => Transaction)) private transactions;
+
     mapping(address => Tracker) private tracker;
     mapping(string => bool) private all_ids;
 
     function addExpense(
-        string memory _id,
         uint256 _amount,
         string memory _description,
         string memory _date,
         string memory _category
     ) public {
-        require(all_ids[_id] == false, "invalid id");
-        transactions[msg.sender][_id] = Transaction(
-            _id,
+        require(bytes(_category).length > 0, "category is required");
+        require(bytes(_date).length > 0, "date is required");
+
+        uint256 id = tracker[msg.sender].counter;
+        tracker[msg.sender].counter += 1;
+
+        Transaction memory expense = Transaction(
+            id,
             TransactionType.EXPENSE,
             _category,
             _description,
@@ -42,21 +48,27 @@ contract ExpenseTracker {
             _date,
             false
         );
+        
+        tracker[msg.sender].transactions[id] = _getTransactionId(id);
+        all_ids[_getTransactionId(id)] = true;
+
         tracker[msg.sender].total += 1;
-        tracker[msg.sender].transactions.push(_id);
-        all_ids[_id] = true;
     }
 
     function addIncome(
-        string memory _id,
         uint256 _amount,
         string memory _description,
         string memory _date,
         string memory _category
     ) public {
-        require(all_ids[_id] == false, "invalid id");
-        transactions[msg.sender][_id] = Transaction(
-            _id,
+        require(bytes(_category).length > 0, "category is required");
+        require(bytes(_date).length > 0, "date is required");
+
+        uint256 id = tracker[msg.sender].counter;
+        tracker[msg.sender].counter += 1;
+
+        Transaction memory income = Transaction(
+            id,
             TransactionType.INCOME,
             _category,
             _description,
@@ -64,28 +76,32 @@ contract ExpenseTracker {
             _date,
             false
         );
+        
+        tracker[msg.sender].transactions[id] = _getTransactionId(id);
+        all_ids[_getTransactionId(id)] = true;
+
         tracker[msg.sender].total += 1;
-        tracker[msg.sender].transactions.push(_id);
     }
 
-    function deleteTransaction(string memory id) public {
-        require(transactions[msg.sender][id].is_deleted == false, "transaction already deleted");
+    function deleteTransaction(uint256 id) public {
+        require(tracker[msg.sender].transactions[id] != "", "transaction not found");
+        require(!transactions[msg.sender][id].is_deleted, "transaction already deleted");
+
         transactions[msg.sender][id].is_deleted = true;
         tracker[msg.sender].deleted += 1;
     }
 
     function getUserTransactions() public view returns (Transaction[] memory) {
-        string[] memory ids = tracker[msg.sender].transactions;
-
-        uint total = tracker[msg.sender].total;
-        uint deleted = tracker[msg.sender].deleted;
-        uint n = total - deleted;
+        uint256 total = tracker[msg.sender].total;
+        uint256 deleted = tracker[msg.sender].deleted;
+        uint256 n = total - deleted;
 
         Transaction[] memory res = new Transaction[](n);
-        for (uint i = 0; i < n; i++) {
-            Transaction memory t = transactions[msg.sender][ids[i]];
-            if (t.is_deleted == false) {
-                res[i] = t;
+        uint256 count = 0;
+        for (uint256 i = 0; i < tracker[msg.sender].counter; i++) {
+            if (bytes(tracker[msg.sender].transactions[i]).length > 0 && !transactions[msg.sender][i].is_deleted) {
+                res[count] = transactions[msg.sender][i];
+                count++;
             }
         }
         return res;
@@ -93,5 +109,28 @@ contract ExpenseTracker {
 
     function getUserTransactionsLen() public view returns (uint256) {
         return tracker[msg.sender].total - tracker[msg.sender].deleted;
+    }
+    
+    function _getTransactionId(uint256 id) private pure returns (string memory) {
+        return string(abi.encodePacked("TX-", uintToString(id)));
+    }
+
+    function uintToString(uint256 value) private pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 }
