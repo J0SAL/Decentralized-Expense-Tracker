443f735a30c3396f2f6bc5f343e93109b3409452
Ojas Aklecha <90605717+ojasaklechayt@users.noreply.github.com>
2023-06-14 21:46:33 +0530

    Another one

--- frontend/components/common/Login.tsx
+++ frontend/components/common/Login.tsx
@@ -4,26 +4,62 @@ import { ConnectButton } from "web3uikit";
 function Login() {
   return (
     <div>
-      <div
+      <div className="wrapper"
         style={{
-          minHeight: "90vh",
           display: "flex",
           justifyContent: "center",
-          flexDirection: "column",
           textAlign: "center",
-          alignContent: "center",
           alignItems: "center",
-          gap: "20px",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          minHeight: "95vh",
         }}
       >
         <img
           src="/images/welcome.svg"
           alt="no data"
-          width={"100vh"}
-          height={"100vh"}
+          width={"300vh"}
+          height={"300vh"}
+          style={{ marginBottom: "1rem" }} // added marginBottom for desktop mode
         />
-        <ConnectButton moralisAuth={false} />
+        <div className="welcome"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            textAlign: "center",
+            alignContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1 style={{ padding: "5px 5px", margin: 0 }}>Welcome to Jarvis</h1>
+          <ConnectButton moralisAuth={false} />
+        </div>
       </div>
+      <div style={{ height: "7vh" }}>
+      </div>
+
+      {/* Media query for mobile mode */}
+      <style>
+        {`
+          .wrapper{
+            gap: 5rem;
+          }
+          @media only screen and (max-width: 600px) {
+            img {
+              padding-top: 2rem; // remove margin-bottom for mobile mode
+            }
+
+            .wrapper{
+              gap:0rem;
+            }
+
+            .welcome{
+              margin-top: -12rem;
+            }
+          }
+        `}
+      </style>
     </div>
   );
 }
