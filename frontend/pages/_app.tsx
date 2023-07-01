db09dff6e571cfa48e95ca2f13136a8e121233d9
Shraddha Singh <shraddhasinghbais4@gmail.com>
2023-07-01 12:34:58 +0530

    Lazy loading is added

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,13 +3,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import React from "react";
 import PageNavbar from "../components/common/PageNavbar";
 import Footer from "../components/common/Footer";
 import DataState from "../context/DataContext/dataState";
 import { SpeechProvider } from "@speechly/react-client";
 import { MoralisProvider } from "react-moralis";
 import { NotificationProvider } from "web3uikit";
+import React from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -24,7 +24,7 @@ export default function App({ Component, pageProps }: AppProps) {
                   <meta name="description" content="Made by Joy Almeida" />
                   <link rel="icon" href="/favicon.ico" />
                 </Head>
-                <PageNavbar />
+                <PageNavbar />  
                 <Component {...pageProps} />
                 <Footer />
               </React.Fragment>
