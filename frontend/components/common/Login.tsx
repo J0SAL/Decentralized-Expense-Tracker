b1661e4c3522516984ad4f084d601fbf96d9a0d8
Bharti Vilas Nagpure <104631882+BhartiNagpure@users.noreply.github.com>
2023-07-29 15:43:24 +0530

    resolve conversation

--- frontend/components/common/Login.tsx
+++ frontend/components/common/Login.tsx
@@ -1,51 +1,43 @@
-import React , {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { ConnectButton } from "web3uikit";
 
 function Login() {
-  const [isSmallScreen, setIsSmallScreen] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      setIsSmallScreen(window.innerWidth <= 600);
-    };
-
-    // Add event listener to track window resize
-    window.addEventListener("resize", handleResize);
-
-    // Initial check for small screen
-    handleResize();
-
-    // Clean up the event listener on unmount
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-
   return (
     <div>
       <div
         style={{
           minHeight: "90vh",
           display: "flex",
-          justifyContent: "center",     
+          justifyContent: "center",
           alignItems: "center",
-          gap: "20px",
-          flexDirection: isSmallScreen ? "column" : "row", 
+          gap: "20px"
         }}
       >
+        <style>
+          {`
+          @media (max-width: 600px) {
+            div {
+              flex-direction: column;
+            }
+          `}
+        </style>
         <img
           src="/images/welcome.svg"
           alt="no data"
-          width={"300vh"}
-          height={"300vh"}
+          width={"400vh"}
+          height={"400vh"}
         />
-        <div 
-        style={{display:'flex',
-         flexDirection:'column', 
-         alignItems:'center'}}>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}>
           <h2 style={{ fontWeight: 'bold' }}>Welcome to Jarvis</h2>
-          <ConnectButton moralisAuth={false}/>
+          <ConnectButton moralisAuth={false} />
         </div>
       </div>
+
     </div>
   );
 }
